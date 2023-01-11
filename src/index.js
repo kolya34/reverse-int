342d7db2d44071983dde62b910b0aec327c40d29
kolya34 <nikolayparaev34@gmail.com>
2023-01-11 17:01:19 +0300

    Enjoy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let abs = Math.abs(n)
+    let result = abs.toString().split('')
+    const result1 = result.pop()
+    const result2 = result.shift()
+    let final_result = result1 + Number(result).toString() + result2
+    return Number(final_result)
 }
